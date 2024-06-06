023f95092c5a9754e8a7cedd6ee65f4bc7dcb2f9
jusing <jusing50a@163.com>
2024-06-06 11:07:32 +0000

    feat: add site metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,19 +11,15 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteTitle: 'Running Page',
+  siteUrl: 'https://jusing50a.github.io/running_page',
+  logo: 'https://avatars.githubusercontent.com/u/37446043?v=4',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://ben29.xyz',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+      name: 'Strava',
+      url: 'https://www.strava.com/athletes/91018631',
     },
   ],
 };
